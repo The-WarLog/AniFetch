4a682f331787132c0c8d2dbf3595f2ea10600f31
DIvyesh <divyeshgaigol16@gmail.com>
2025-08-02 22:37:52 +0530

    html changes

--- routes/animeroutes.js
+++ routes/animeroutes.js
@@ -2,20 +2,14 @@ var express = require('express');
 var router = express.Router();
 var animeController = require('../controllers/animecontroller');
 
-// Root route for /anime
+// Root route for /anime/
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Anime API root' });
 });
 
-
-
-
+// More specific routes first to avoid overlap
 router.get('/current-season', animeController.GetCurrentSeasonAnime);
-
-router.get('/search/:name', animeController.GetParticularAnimeInfo);
-
 router.get('/:year/:season', animeController.GetAnimeByYearAndSeason);
-
-
+router.get('/search/:name', animeController.GetParticularAnimeInfo);
 
 module.exports = router;
